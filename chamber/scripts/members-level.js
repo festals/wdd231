1e1c7b44c7b509168c9c87b4ec154f745cd8efe4
festals <festals90@gmail.com>
2024-10-07 11:17:56 +0200

    shuffle and 3 random compagny in js file done, the cta on banner

--- chamber/scripts/members-level.js
+++ chamber/scripts/members-level.js
@@ -1,47 +1,44 @@
 const url = "data/members.json";
-const cards = document.getElementById("cards");
+const cards = document.getElementById("filtered-cards");
 
-async function getMemberLevelData() {
+async function getData() {
     const response = await fetch(url);
     const data = await response.json();
-    displayMembersLevel(data.members);
+    displayMembers(data.members.filter(member => member.membershipLevel <= 2).sort(() => 0.5 - Math.random()).slice(0,3));
 }
 
-const getLevel = (members) =>{
-    return members.filter(member => member.membershipLevel === 1|| member.membershipLevel === 2);
-};
+getData(url);
 
-const getRandom = (members, num) => {
-    let shuffled = members.sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, num);
-}
+// function shuffle(members) {
+//     let shuffled = members.sort(() => 0.5 - Math.random());
+//     return shuffled.slice(0, 3);
+// }
 
-const displayMembersLevel = (members) => {
-    const goldAndSilver = getLevel(members);
-    const randomCompanies = getRandom(goldAndSilver, 3);
-
-    members.forEach(compagnie =>{
-        const card = document.createElement("div");  //or let
+const displayMembers = (members) => {
+    
+    cards.innerHTML="";
+    
+    members.forEach((member) =>{
+        let card = document.createElement('div');
         card.classList.add("card");
-        const compagnieLevel = document.createElement("p");
+        const level = document.createElement("p");
+
+        level.innerHTML =`Membership level: ${member.membershipLevel}`;
+
+        if (member.membershipLevel == 1){
+                level.innerHTML = `Membership level: Gold`
+            } else if (member.membershipLevel == 2) {
+                level.innerHTML = `Membership level: Silver`
+            }
 
         card.innerHTML=`
-                    <img src ="images/${compagnie.image}" alt="${compagnie.name}">
-                    <p><b>${compagnie.name}</b></p>
-                    <p>${compagnie.address}</p>
-                    <p>${compagnie.phone}</p>
-                    <p><a href="${compagnie.website}" target="_blank">${compagnie.website}</a></p>
-                    `; 
-        
-        if (compagnie.membershipLevel === 1){
-            compagnieLevel.innerHTML = `Membership level: Gold`
-        } else if (compagnie.membershipLevel === 2) {
-            compagnieLevel.innerHTML = `Membership level: Silver`
-        }
-
-        card.appendChild(compagnieLevel);
+                    <img src ="images/${member.image}" alt="${member.name}">
+                    <p><b>${member.name}</b></p>
+                    <p>${member.address}</p>
+                    <p>${member.phone}</p>
+                    <p><a href="${member.website}" target="_blank">${member.website}</a></p>
+                    `;
+        card.appendChild(level);
         cards.appendChild(card);
-    })
-}
-
-getMemberLevelData(url);
+    });
+};
